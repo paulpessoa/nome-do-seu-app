1977cd41b40a59ef6ab130e1b018afe8da14bb42
Paul Pessoa <paulmspessoa@gmail.com>
2024-04-25 11:16:41 -0300

    instalando pacotes

--- pages/components/PostForm.tsx
+++ pages/components/PostForm.tsx
@@ -2,21 +2,21 @@ import React, { useRef, useState } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Image from '@tiptap/extension-image';
-import Blockquote from '@tiptap/extension-blockquote';
+// import Blockquote from '@tiptap/extension-blockquote';
 import Bold from '@tiptap/extension-bold';
-import BulletList from '@tiptap/extension-bullet-list';
-import Code from '@tiptap/extension-code';
-import CodeBlock from '@tiptap/extension-code-block';
-import Dropcursor from '@tiptap/extension-dropcursor';
-import Gapcursor from '@tiptap/extension-gapcursor';
-import HardBreak from '@tiptap/extension-hard-break';
-import Heading from '@tiptap/extension-heading';
-import HorizontalRule from '@tiptap/extension-horizontal-rule';
+// import BulletList from '@tiptap/extension-bullet-list';
+// import Code from '@tiptap/extension-code';
+// import CodeBlock from '@tiptap/extension-code-block';
+// import Dropcursor from '@tiptap/extension-dropcursor';
+// import Gapcursor from '@tiptap/extension-gapcursor';
+// import HardBreak from '@tiptap/extension-hard-break';
+// import Heading from '@tiptap/extension-heading';
+// import HorizontalRule from '@tiptap/extension-horizontal-rule';
 import Italic from '@tiptap/extension-italic';
-import ListItem from '@tiptap/extension-list-item';
-import OrderedList from '@tiptap/extension-ordered-list';
-import Paragraph from '@tiptap/extension-paragraph';
-import Strike from '@tiptap/extension-strike';
+// import ListItem from '@tiptap/extension-list-item';
+// import OrderedList from '@tiptap/extension-ordered-list';
+// import Paragraph from '@tiptap/extension-paragraph';
+// import Strike from '@tiptap/extension-strike';
 import Text from '@tiptap/extension-text';
 import { ToggleButton, ToggleButtonGroup, Button, TextField } from '@mui/material';
 import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
@@ -31,22 +31,22 @@ const PostForm = () => {
         extensions: [
             StarterKit,
             Image,
-            Blockquote,
             Bold,
-            BulletList,
-            Code,
-            CodeBlock,
-            Dropcursor,
-            Gapcursor,
-            HardBreak,
-            Heading,
-            HorizontalRule,
             Italic,
-            ListItem,
-            OrderedList,
-            Paragraph,
-            Strike,
             Text,
+            // Blockquote,
+            // BulletList,
+            // Code,
+            // CodeBlock,
+            // Dropcursor,
+            // Gapcursor,
+            // HardBreak,
+            // Heading,
+            // HorizontalRule,
+            // ListItem,
+            // OrderedList,
+            // Paragraph,
+            // Strike,
         ],
         content: '<p>Hello World! 🌎️</p>',
     });
